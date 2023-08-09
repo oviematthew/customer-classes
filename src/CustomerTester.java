b476f3997cb7e883fdc5524aa0290ea53ae7b928
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-08-09 00:00:58 -0400

    changes made

--- src/CustomerTester.java
+++ src/CustomerTester.java
@@ -1,5 +1,9 @@
 public class CustomerTester {
     public static void main(String[] args) throws Exception {
-        
+        System.out.println("Welcome to the Customer tester!");
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        System.out.println("This program will instantiate objects of the Customer hierarchy and test their methods");
+        System.out.println("---------------------------------------------------------------------------------------");
     }
 }
