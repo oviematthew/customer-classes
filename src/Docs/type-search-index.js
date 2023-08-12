56764309888dad57d4c37fd969fa7e2e241b1db2
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2023-08-12 04:08:57 -0400

    generated javadocs

---
+++ src/Docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BusinessCustomer"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"PreferredCustomer"},{"p":"<Unnamed>","l":"RetailCustomer"}];updateSearchResults();
