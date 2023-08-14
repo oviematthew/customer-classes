e7fbc4726f366f5a6f9e50a234e6d597271b00ff
Matthew Ovie Enamuotor <90224141+oviematthew@users.noreply.github.com>
2023-08-14 19:48:48 +0100

    Delete bin/Docs directory

--- bin/Docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BusinessCustomer"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"PreferredCustomer"},{"p":"<Unnamed>","l":"RetailCustomer"}];updateSearchResults();
